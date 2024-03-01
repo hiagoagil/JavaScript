0db26c06c4f72bc64aa5c318b585fd185307b717
hiagogil <hiago-gil@hotmail.com>
2024-02-29 21:27:00 -0300

    first commit

---
+++ index/index8.js
@@ -0,0 +1,9 @@
+var vel = 6
+
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+
+if ( vel > 60 ) {
+    console.log(`Voce ultrapassou a velocidade permitida`)
+}
+
+console.log(`Dirija sempre utilizando a cinto de segurança`)
---
+++ index/index9.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log (`Se você está no Brasil e o seu filho nasceu no ${pais} ele é ` )
+if (pais == 'Brasil') {
+    console.log ('Brasileiro')
+} else {
+    console.log ('Estrangeiro')
+}
