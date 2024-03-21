f0c407a19c953c76b00a94ca5b0d23b7903b4db0
hiagogil <hiago-gil@hotmail.com>
2024-03-21 15:38:50 -0300

    while

---
+++ index/index14.js
@@ -0,0 +1,20 @@
+var c = 1
+// while (c <= 3) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+do {
+    console.log(`Vou ser uma grande referência de Q.A ${c}`)
+    c++
+}
+while (c>=6)
