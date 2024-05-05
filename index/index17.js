e9ad961940be3eedc3977eea0c014ce00b8b5c5b
Hiago <hiagoqagil@gmail.com>
2024-05-05 19:36:45 -0300

    Funções I

---
+++ index/index17.js
@@ -0,0 +1,22 @@
+function parouimpar(n) {
+    if (n%2==0){
+         return 'par'}
+     else {
+       return 'impar'
+    }
+}
+
+console.log(parouimpar(223))
+
+function soma (n1,n2) {
+    return n1 + n2}
+
+console.log(soma(1,22))
+
+let v = function (x) {
+    return x*2
+}
+console.log(v(10))
+
+
+func
